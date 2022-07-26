ac4cdd37dd9a8b3e90c2487c83aa1f01f7b44b2f
Michał Walter <waltermichal92@gmail.com>
2022-07-26 18:02:19 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from os import environ
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -6,8 +6,11 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'ThisK3Y$houldB3S3cr3t'
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or 'sqlite:///Webpollapp_Database.db'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') #or 'ThisK3Y$houldB3S3cr3t'
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri #or 'sqlite:///Webpollapp_Database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
